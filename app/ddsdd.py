3cd949e1ec31017c91a4516c7926e37c03545a2a
Tristan Pfuderer <pfudi200@gmail.com>
2026-01-28 13:23:56 +0100

    Make rag a proper Python package for Streamlit Cloud

--- app/ddsdd.py
+++
@@ -1,9 +0,0 @@
-from google import genai
-import os
-
-client = genai.Client(api_key=os.environ["GOOGLE_API_KEY"])
-
-print("All models visible to this API key:\n")
-
-for model in client.models.list():
-    print(model.name)
